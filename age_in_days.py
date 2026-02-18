7e010fd02fb6fbad326f36ba62c8826ec9b3b7b5
jbrod02 <jbrodi02@gmail.com>
2026-02-17 22:13:58 -0600

    More learning, created an age calculator

---
+++ age_in_days.py
@@ -0,0 +1,4 @@
+age = input("How old are you (in years) ?")
+days = float(age) * 365
+print(str(days) + " days old")
+
--- learning.py
+++ learning.py
@@ -1,8 +1,9 @@
-# message = "Pickler\nTickler\\"
-# print(message)
 
-'''
-hello
-world
+print("!*!*!WELCOME TO THE NAME APP!*!*!*")
 
-'''
+first_name = input("What is yout first name??")
+last_name = input("What is your last name??")
+
+full_name = first_name + " " + last_name
+
+print("Hi there," + full_name + "!!")
